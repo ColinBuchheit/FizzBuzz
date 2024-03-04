645b0a63faff40937fd8fb45f66165b3681d823e
Colin Buchheit <130728626+ColinBuchheit@users.noreply.github.com>
2024-03-04 16:03:08 -0600

    Update FizzbuzPython.py
    This is my first quality improvement for the efficiency of the program

--- FizzbuzPython.py
+++ FizzbuzPython.py
@@ -1,18 +1,8 @@
 
-#FizzBuzz Program
-#Iterates from 1 to 100, printing "Fizz" for multiples of 3, "Buzz" for multiples of 5,
-#and "FizzBuzz" for multiples of both 3 and 5. For all other numbers, it prints the number itself.
-
-for i in range(1, 101):  #loop through numbers 1 to 100 (inclusive)
-    output = ''  #initializes an empty string to hold the output for each number
-
-    #checks for multiples of 3
+for i in range(1, 101): 
+    output = ''  
     if i % 3 == 0:
-        output += 'Fizz'  #checks the current number "i" of it is divisable by 3. If true it outputs Fizz
-
-    #checks for multiples of 5
+        output += 'Fizz' 
     if i % 5 == 0:
-        output += 'Buzz'  #checks if the "i" os divisable by 5, if so, it outputs the Buzz
-
-    #prints the output if not empty; otherwise, print the number itself
+        output += 'Buzz'  
     print(output or i)
